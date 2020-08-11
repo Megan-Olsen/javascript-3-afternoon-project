18f2f19e6d34b591a1230e1c9e2f1ed4b5ac5b82
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-10 18:54:06 -0600

    nesting fun

--- nesting.js
+++ nesting.js
@@ -81,7 +81,7 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 //Code Here
 const removeDuplicates = () => {
   for(let i = 0; i < workplaceAccidents.length; i ++ ){
-    for(let j=i +1; j < workplaceAccidents.length; j++){
+    for(let j = i + 1; j < workplaceAccidents.length; j++){
       console.log(workplaceAccidents[i], workplaceAccidents[j])
       if(workplaceAccidents[i] === workplaceAccidents[j]){
         workplaceAccidents.splice(i,1)
